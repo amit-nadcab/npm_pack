adabaa5be18b1b82b1ca00c98e36efbd602a6417
amit-nadcab <amitnadcab@gmail.com>
2022-10-27 23:27:18 +0530

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+function paintItRed(string) {
+    // const text = string;
+    alert(string)
+    // const a = document.createElement("p");
+    // a.style.color = "red";
+    // a.innerHTML = text;
+    // document.body.appendChild(a);
+    }
+
+    module.exports.paintItRed = paintItRed;
